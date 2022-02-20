e25d6818017c82d22e883de0b8043ae68c05fe6b
Elizabeth <elizabeth.por@gmail.com>
2022-02-19 20:33:01 -0600

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Liza loves living in the Big Apple because it's a walkable city that has so much to offer.</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  )
 }
 
 export default About;
